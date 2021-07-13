a24126ad4ebc1c2b1a0be3205dc34a2c4e0352cc
vikeju <virginie.grolleau+github@protonmail.com>
2021-07-13 11:45:28 +0200

    Démo passage de variables à la vue

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -18,6 +18,14 @@ class MainController extends AbstractController {
      * @Route("/test", name="main_test")
      */
     public function test() {
-        return $this->render('main/test.html.twig');
+        $serie = [
+            "title" => "Game of Thrones",
+            "year" => 2000
+        ];
+
+        return $this->render('main/test.html.twig', [
+            "mySerie" => $serie,
+            "autreVar" => 412412
+        ]);
     }
 }
