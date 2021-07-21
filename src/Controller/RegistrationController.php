3643536bc19c521a2a4f91c8e58685bf2f4c849a
vikeju <virginie.grolleau+github@protonmail.com>
2021-07-21 11:18:19 +0200

    Démo authentification utilisateur

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -20,6 +20,7 @@ class RegistrationController extends AbstractController
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, UserAuthenticatorInterface $authenticator, AppAuthenticator $formAuthenticator): Response
     {
         $user = new User();
+        $user->setRoles(["ROLE_USER"]);
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
